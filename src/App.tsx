5f761f1863d2da26d9ad3b2604063ce3e1e673dd
MarcilenoReda <marcilenoreda120@gmail.com>
2025-10-15 21:35:38 +0300

    Add Vercel Analytics for tracking visitors and page views

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Landing from './Landing';
 import AdminLayout from './admin/AdminLayout';
 import RequireAdmin from './admin/RequireAdmin';
@@ -8,24 +9,27 @@ import ProjectForm from './admin/ProjectForm';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/admin/login" element={<Login />} />
-      <Route
-        path="/admin"
-        element={
-          <RequireAdmin>
-            <AdminLayout />
-          </RequireAdmin>
-        }
-      >
-        <Route index element={<Navigate to="projects" replace />} />
-        <Route path="projects" element={<ProjectsList />} />
-        <Route path="projects/new" element={<ProjectForm />} />
-        <Route path="projects/:id/edit" element={<ProjectForm />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/admin/login" element={<Login />} />
+        <Route
+          path="/admin"
+          element={
+            <RequireAdmin>
+              <AdminLayout />
+            </RequireAdmin>
+          }
+        >
+          <Route index element={<Navigate to="projects" replace />} />
+          <Route path="projects" element={<ProjectsList />} />
+          <Route path="projects/new" element={<ProjectForm />} />
+          <Route path="projects/:id/edit" element={<ProjectForm />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
